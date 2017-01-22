e8fd296a893aa4a7359f7842f1329b18eeae402d
Sian Nadin <nadin.sian@gmail.com>
2017-01-22 19:04:37 +0000

    listview set up with placeholder data

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -1,9 +1,12 @@
 package com.example.android.quakereport;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -45,7 +48,38 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
      */
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
-        
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link AndroidFlavor} object located at this position in the list
+        Earthquake currentEarthquake = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID magnitude
+        TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.magnitude);
+        // Get the magnitude from the current Earthquake object and
+        // set this text on the magnitude TextView
+        String magnitudeDouble= Double.toString(currentEarthquake.getMagnitude());
+        magnitudeTextView.setText(magnitudeDouble);
+
+        // Find the TextView in the list_item.xml layout with the ID location
+        TextView locationTextView = (TextView) listItemView.findViewById(R.id.location);
+        // Get the location from the current Earthquake object and
+        // set this text on the location TextView
+        locationTextView.setText(currentEarthquake.getLocation());
+
+        // Find the TextView in the list_item.xml layout with the ID date
+        TextView dateTextView = (TextView) listItemView.findViewById(R.id.date);
+        // Get the date from the current Earthquake object and
+        // set this text on the date TextView
+        dateTextView.setText(currentEarthquake.getDate());
+
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return listItemView;
 
     }
 
